585a7b07f8433f6006c30b4aff06ad96517f93fc
Alexandr <paladinen1@gmail.com>
2019-03-10 22:08:45 +0300

    добавлен функционал добавления

--- app/src/main/java/android/itacademy/by/dz6/dz6Activity.java
+++ app/src/main/java/android/itacademy/by/dz6/dz6Activity.java
@@ -1,20 +1,27 @@
 package android.itacademy.by.dz6;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.itacademy.by.menu.R;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-public class dz6Activity extends Activity {
+public class dz6Activity extends Activity implements View.OnClickListener {
     private RecyclerView rView;
     private StudentAdapter adapter = new StudentAdapter();
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dz6_layout);
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+        fab.setAlpha(0.5f);
         rView = findViewById(R.id.recyclerDz6);
         rView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -33,5 +40,11 @@ public class dz6Activity extends Activity {
     protected void onPause() {
         adapter.notifyDataSetChanged();
         super.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this,CreateStudentActivity.class);
+        startActivity(intent);
     }
 }
