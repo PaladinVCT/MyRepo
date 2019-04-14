b96e7aae79923ee507983c00b8bb662afadc0a09
Alexandr Lebedev <paladinen1@gmail.com>
2019-04-14 22:24:52 +0300

    добавлека стрелка

--- app/src/main/java/android/itacademy/by/dz14/activities/DetailsActivity.kt
+++ app/src/main/java/android/itacademy/by/dz14/activities/DetailsActivity.kt
@@ -1,6 +1,13 @@
 package android.itacademy.by.dz14.activities
 
+import android.itacademy.by.menu.R
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class DetailsActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.person_details_layout)
+    }
 }
